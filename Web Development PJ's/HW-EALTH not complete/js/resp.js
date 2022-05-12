b2e64a7339e3907a71708d6a2ca6eaf01d2f6670
Shubham Kumar <77887476+acroshubham@users.noreply.github.com>
2022-05-12 15:11:18 +0530

    Add files via upload

---
+++ Web Development PJ's/HW-EALTH not complete/js/resp.js
@@ -0,0 +1,11 @@
+burger=document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+navlist = document.querySelector('.navlist')
+burger=document.querySelector('.burger')
+
+burger.addEventListener('click',()=>{
+    navbar.classList.toggle('vclassresp');
+    navbar.classList.toggle('vclassresp');
+    navlist.classList.toggle('navheightresp');
+})
+
